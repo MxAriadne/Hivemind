7746a9ea2984ad5306d458321afa04b164dca3a3
Kennedy Bowles <kennedy.bowles@yahoo.com>
2023-11-28 13:23:47 -0600

    frontend

--- src/main/java/com/hivemind/controllers/MainController.java
+++ src/main/java/com/hivemind/controllers/MainController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.animation.FadeTransition;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -14,6 +16,21 @@ public class MainController {
 
     @FXML
     Button pair_pc, hide_window;
+
+
+    @FXML
+    protected void initialize() {
+        applyFadeTransition(pair_pc);
+        applyFadeTransition(hide_window);
+    }
+
+    private void applyFadeTransition(Button button) {
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), button);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.5);
+        fadeTransition.play();
+    }
+
 
     @FXML
     private void handleButtonClick(ActionEvent e) throws IOException {
--- src/main/java/com/hivemind/controllers/PairController.java
+++ src/main/java/com/hivemind/controllers/PairController.java
@@ -2,13 +2,16 @@ package com.hivemind.controllers;
 
 import com.hivemind.DatabaseConn;
 import com.hivemind.SceneController;
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -22,8 +25,14 @@ public class PairController {
     Button cancel;
 
     @FXML
-    protected void initialize() {
+    Text textPair;
 
+    @FXML
+    protected void initialize() {
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), textPair);
+        fadeTransition.setFromValue(0.0); // Start fully transparent
+        fadeTransition.setToValue(1.0);   // Fade to fully opaque
+        fadeTransition.play();
     }
 
     @FXML
